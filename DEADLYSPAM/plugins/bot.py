2f92faa5c1583ee6e39a17735bd7c4e6c39321ce
SATYAM <120216646+RAICHU07@users.noreply.github.com>
2023-01-09 02:35:12 +0530

    Update bot.py

--- DEADLYSPAM/plugins/bot.py
+++ DEADLYSPAM/plugins/bot.py
@@ -15,10 +15,10 @@ from telethon.tl.custom import button
 from time import time
 from datetime import datetime
 
-DEAD_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/c6f99c0b68ff07439ed72.jpg"
+DEAD_PIC = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/15b7c17ecd2d7e65094fc.jpg"
   
 
-DEADLY = "✯ 𝗗𝗲𝗮𝗱𝗹𝘆 𝗦𝗽𝗮𝗺 𝗛𝗲𝗿𝗲 ✯\n\n"
+DEADLY = "✯ HOTSPOT 𝗦𝗽𝗮𝗺 𝗛𝗲𝗿𝗲 ✯\n\n"
 DEADLY += f"═══════════════════\n"
 DEADLY += f"• **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.10.1`\n"
 DEADLY += f"• **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
@@ -32,10 +32,10 @@ async def alive(event):
      await BOT0.send_file(event.chat_id,
                                   DEAD_PIC,
                                   caption=DEADLY,
-                                  buttons=[
+                                 buttons=[
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/DEADLY_SPAMBOT"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/DEADLY_SPAM_BOT")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/HOTSPOTXMUSIC"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/HOTSPOTXMUSIC")
         ],
         [
         Button.url("• ʀᴇᴘᴏ •", "https://github.com/Team-Deadly/DEADLY-SPAMBOT")
